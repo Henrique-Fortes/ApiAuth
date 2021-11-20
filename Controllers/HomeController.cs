930b1b5a4cd3297283ccdbfc3ccdd70ce70b715e
Henrique H. da Silva Fortes <Henrique Fortes>
2021-11-20 12:36:49 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ApiAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ApiAuth.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anonino";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => $"Autenticado - {User.Identity.Name}";
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee, manager")]
+        public string Employee() => "Funcionario";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiAuth
+{
+    public static class Settings
+    {
+        public static string Secret = "K3OIuwcqf4zpGCak6fkICm3FrECA4nqPdfd";
+    }
+}
